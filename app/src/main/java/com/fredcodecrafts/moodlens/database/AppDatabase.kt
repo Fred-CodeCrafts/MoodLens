0e8facd4491e9b0698d2b833c82b497be79acf88
chmpgnsupernova <brianmariarvin@gmail.com>
2025-10-18 11:56:48 +0700

    hehe

--- app/src/main/java/com/fredcodecrafts/moodlens/database/AppDatabase.kt
+++ app/src/main/java/com/fredcodecrafts/moodlens/database/AppDatabase.kt
@@ -18,7 +18,7 @@ import com.fredcodecrafts.moodlens.database.entities.*
         Question::class,
         MoodScanStat::class
     ],
-    version = 2, // ⬅️ incremented because we added a new column
+    version = 3, // ⬅️ incremented because we added a new column
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -40,6 +40,12 @@ abstract class AppDatabase : RoomDatabase() {
                 database.execSQL("ALTER TABLE journal_entries ADD COLUMN location TEXT")
             }
         }
+        private val MIGRATION_2_3 = object : Migration(2, 3) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                // Tambahkan kolom baru aiReflection
+                database.execSQL("ALTER TABLE journal_entries ADD COLUMN aiReflection TEXT")
+            }
+        }
 
         fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
@@ -49,8 +55,7 @@ abstract class AppDatabase : RoomDatabase() {
                     "emotion_journal_db"
                 )
                     // 🔧 Register the migration here
-                    .addMigrations(MIGRATION_1_2)
-                    .build()
+                    .addMigrations(MIGRATION_1_2, MIGRATION_2_3)                    .build()
                 INSTANCE = instance
                 instance
             }
